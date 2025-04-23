4798fc4729edfd7730e78536ee029605af8708ae
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-04-23 16:30:18 +0100

    added parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::error::Error;
+
+/// Simple Git repo auto-updater.
+#[derive(Parser)]
+struct Cli {
+    /// Path to config TOML
+    #[arg(short, long, default_value = "watcher.toml")]
+    config: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    println!("{}", args.config);
+    Ok(())
 }
